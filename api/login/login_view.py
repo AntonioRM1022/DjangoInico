1f57096579e058ff4e43aa378b26d5fa5fa1616c
AntonioRM1022 <antonioreach102@gmail.com>
2024-10-24 01:30:07 -0600

    Modificado2

--- api/login/login_view.py
+++ api/login/login_view.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
-def login_view(requets):
-    template_view="lauth-login.html"
+def login_view(request):
+    template_name="login.html"
     
-    return render(requets, template_name=template_view)
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user) 
+            return redirect('home')
+        else:
+            return render (request, template_name , {'error': 'Credenciales inválidas'})
+    return render(request, template_name=template_name)
+# view for register.
+
+def register_view(request):
+    template_view="register.html"
+    return render(request, template_name=template_view)
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
